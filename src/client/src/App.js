dbaa40f9ddee6f975fa8b50c48f524b003ffe33f
abbyswag <abbyswag25@gmail.com>
2021-03-19 20:19:14 +0530

    boiler-plate

---
+++ src/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header/header'
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+        <Header/>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
